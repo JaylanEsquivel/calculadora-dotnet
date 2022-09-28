0f5aef642de043e1242b22d81c3558c8ea1234f3
JaylanEsquivel <jaylan1997@hotmail.com>
2022-09-28 14:41:23 -0300

    Raiz quadrada

--- Models/Calculadora.cs
+++ Models/Calculadora.cs
@@ -58,6 +58,9 @@ namespace calculadora.Models
             Console.WriteLine($"Tangente de {angulo}° = {Math.Round(tangente,4)}");
         }
 
-        
+        public void RaizQuadrada(double x){
+            double raiz = Math.Sqrt(x);
+            Console.WriteLine($"Raiz quadrada de {x} = {Math.Round(raiz, 2)}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -12,3 +12,4 @@ calculo.Potencia(5,5);
 calculo.Seno(150);
 calculo.Coseno(80);
 calculo.Tangente(20);
+calculo.RaizQuadrada(20);
